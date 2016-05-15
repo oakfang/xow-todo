442ee2de7f24129270d9d0025a8b211077740deb
Alon Niv <oakfang@gmail.com>
2016-05-15 11:27:43 +0300

    task filter

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const {YES, NO, dom} = require('xow');
+const {push} = require('xow-route');
 
 const App = require('./base');
 
@@ -11,10 +12,13 @@ module.exports = class Task extends App {
                             Object.assign({}, task, {enabled: !task.enabled}),
                             ...this.state.tasks.slice(i + 1)];
     }
+    filterTo() {
+        push(`/list/${this.props.task.id}`);
+    }
     render() {
         const {task} = this.props;
         return (
-            <li key={task.id} style={{'list-style-type': 'none', cursor: 'pointer'}} onclick={() => this.toggleTask()}>
+            <li key={task.id} style={{'list-style-type': 'none', cursor: 'pointer'}} onclick={this.toggleTask} ondblclick={this.filterTo}>
                 <input type="checkbox"
                        checked={task.enabled ? NO : YES} />
                 <span class="task-text" style={{'text-decoration': task.enabled ? 'initial' : 'line-through'}}>{task.text}</span>
